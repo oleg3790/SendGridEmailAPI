292b149760b905cb55f59f1ba05acc4dd5450bdb
oleg3790 <oleg.krysko@outlook.com>
2018-08-31 12:26:09 -0500

    Misc Cleanup

---
+++ SendGridEmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SendGridEmailAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        // Test method
+        [HttpGet]
+        public string Get()
+        {
+            return "Success";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
