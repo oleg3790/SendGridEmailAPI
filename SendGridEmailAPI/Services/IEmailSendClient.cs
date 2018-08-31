5aa07fd5f076f232e3c4d69f078b06d20103863f
oleg3790 <oleg.krysko@outlook.com>
2018-08-31 13:12:25 -0500

    Forgot to stage previous changes

---
+++ SendGridEmailAPI/Services/IEmailSendClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SendGridEmailAPI.Services
+{
+    public interface IEmailSendClient
+    {
+        Task SendEmailAsync(string fromEmail, string fromEmailName, string toEmail, string subject, string message);
+    }
+}
---
+++ SendGridEmailAPI/Services/SendGridEmailClient.cs
@@ -0,0 +1,30 @@
+﻿using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace SendGridEmailAPI.Services
+{
+    public class SendGridEmailClient : IEmailSendClient
+    {
+        private readonly string _apiKey;
+
+        public SendGridEmailClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public Task SendEmailAsync(string fromEmail, string fromEmailName, string toEmail, string subject, string message)
+        {
+            var sendGridMessage = new SendGridMessage();
+            sendGridMessage.AddTo(toEmail);
+            sendGridMessage.From = new EmailAddress(fromEmail, fromEmailName);
+            sendGridMessage.Subject = subject;
+            sendGridMessage.HtmlContent = message;
+
+            SendGrid.ISendGridClient transportWeb = new SendGrid.SendGridClient(_apiKey);
+            transportWeb.SendEmailAsync(sendGridMessage);
+
+            return Task.CompletedTask;
+        }
+    }
+}
